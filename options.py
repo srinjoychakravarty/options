a15226770bef9288245289672f02eebbfecd5765
Srinjoy Chakravarty <chakravarty.s@northeastern.edu>
2021-03-17 03:14:07 -0400

    :clamp: refactored code to create filter calls and puts as reusable function

--- options.py
+++ options.py
@@ -25,14 +25,46 @@ def state_price_density(c_0303, strike_0303, st):
     
     return SPD
 
+def filter_calls_from_puts(contracts_worksheet):
+    '''populates individual lists for call and put contracts'''
+   
+    rowcount = contracts_worksheet.nrows
+    contract_types = []                                                 # list of option types for 3 Mar                                
+    for i in range (1, rowcount):
+        contract_types.append(contracts_worksheet.cell_value(i, 3))
+    
+    call_contracts = []
+    put_contracts = []
 
+    for i in range (1, rowcount):
+        if ("C" in contract_types[i - 1]):
+            call_contracts.append(contracts_worksheet.row(i))
+        else:
+            put_contracts.append(contracts_worksheet.row(i))
+
+    return (call_contracts, put_contracts)
+    
+
+def list_bids():
+    ''' '''
+
+def list_asks():
+    ''' '''
+
+def list_bid_ask_call_spreads(call_contract_count, bid_list, ask_list):
+    '''returns a list of bid-ask spreads for call options for a given expiry date'''
+    call_spreads = []
+    for i in range (0, call_contract_count):
+        call_spreads.append( ( ( bid_list[i] + ask_list[i] ) / 2) )
+    return call_spreads
+    
 if __name__ == "__main__":
     directory = os.getcwd()
     filepath = f"{directory}/OPTIONS.xls"
     workbook = xlrd.open_workbook(filepath, on_demand = True)
     sheet_names = workbook.sheet_names()
     worksheets = []
-    for i in range (0, len(sheet_names) - 1):
+    for i in range (0, len(sheet_names)):
         worksheets.append({sheet_names[i]: workbook.sheet_by_index(i)})
 
     # st = state, total market value (top 500 US Securities) at time T (Mar 3, 2021)
@@ -40,54 +72,42 @@ if __name__ == "__main__":
 
     # analysis for option contracts expiring Mar 3, 2021
     options_expiring_0303 = worksheets[0].get('0303')
-    options_expiring_0303_rowcount = options_expiring_0303.nrows
+  
+    (call_contracts, put_contracts)  = filter_calls_from_puts(options_expiring_0303)
 
-    # list of option types for 3 Mar
-    type_0303 = []                                                     
-    for i in range (1, options_expiring_0303_rowcount):
-        type_0303.append(options_expiring_0303.cell_value(i, 3))
-    # print(f"Option Types (March 3): {type_0303}")
-
-    call_contracts = []
-    put_contracts = []
-    for i in range (1, options_expiring_0303_rowcount):
-        if ("C" in type_0303[i - 1]):
-            call_contracts.append(options_expiring_0303.row(i))
-        else:
-            put_contracts.append(options_expiring_0303.row(i))
-
-    call_contract_count = len(call_contracts)
-    put_contract_count = len(put_contracts)
+    put_contract_count_0303 = len(put_contracts)
+    call_contract_count_0303 = len(call_contracts)
 
     # list of STRIKE PRICES for CALL options on 3 Mar. 2021
     strike_0303 = []
-    for i in range (0, call_contract_count):
+    for i in range (0, call_contract_count_0303):
         strike_0303.append(call_contracts[i][2].value)
     # print(f"Strike Prices (Calls): {strike_0303}")
 
     # list of IMPLIED VOLATILITIES for CALL options on 3 Mar. 2021
     implied_vol_0303 = []
-    for i in range (0, call_contract_count):
+    for i in range (0, call_contract_count_0303):
         implied_vol_0303.append(call_contracts[i][11].value)
     # print(f"Implied Volatilties (Calls): {implied_vol_0303}")
 
     # list of BIDS for CALL options on 3 Mar. 2021
-    bid_0303 = []
-    for i in range (0, call_contract_count):
-        bid_0303.append(call_contracts[i][5].value)
+    bid_list_0303 = []
+    for i in range (0, call_contract_count_0303):
+        bid_list_0303.append(call_contracts[i][5].value)
     # print(f"Bids (Calls): {bid_0303}")
 
     # list of ASKS for CALL options on 3 Mar. 2021
-    ask_0303 = []
-    for i in range (0, call_contract_count):
-        ask_0303.append(call_contracts[i][6].value)
+    ask_list_0303 = []
+    for i in range (0, call_contract_count_0303):
+        ask_list_0303.append(call_contracts[i][6].value)
     # print(f"Asks (Calls): {ask_0303}")
 
+    ask_list_0303 = list_asks()
+
     # list of BID-ASK SPREADS for CALL options on 3 Mar. 2021
-    c_0303 = []
-    for i in range (0, call_contract_count):
-        c_0303.append( ( ( bid_0303[i] + ask_0303[i] ) / 2) )
-    # print(f"Bid Ask Spread (Calls): {c_0303}")
+    c_0303 = list_bid_ask_call_spreads(call_contract_count_0303, bid_list_0303, ask_list_0303)
 
     spd_0303 = state_price_density(c_0303, strike_0303, st)
-    print(f"SPD: {spd_0303}")
+    print(spd_0303)
+    
+    # print(worksheets)
